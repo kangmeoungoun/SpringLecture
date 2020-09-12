3af8b280d6d7e3cd276729667e2ed5d33bedd721
kmo <kes98201@gmail.com>
2020-09-12 18:43:22 +0900

    master branch chage a code

--- CommandObject/src/kr/co/softcampus/controller/TestController.java
+++ CommandObject/src/kr/co/softcampus/controller/TestController.java
@@ -14,6 +14,7 @@ public class TestController {
 		// System.out.printf("data1 : %s\n",bean.getData1());
 		// System.out.printf("data2 : %s\n",bean.getData2());
 		System.out.println("!!!!");
+		System.out.println("@@@");
 		return "test1";
 	}
 
